1e93e46a62dba88fe1efa375ebf126380fb9abbd
songtianming <songtianming@didichuxing.com>
2018-11-18 17:19:48 +0800

    update

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net"
+)
 
 func main() {
 	r := gin.Default()
+	var ip string
+	if addrs, err := net.InterfaceAddrs(); err == nil {
+		for _, address := range addrs {
+			// 检查ip地址判断是否回环地址
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					ip =  ipnet.IP.String()
+				}
+
+			}
+		}
+	}
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"version": "1.0",
+			"ip":      ip,
 		})
 	})
 	r.Run("0.0.0.0:8000")
